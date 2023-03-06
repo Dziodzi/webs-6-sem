1a767213bd4bf35208816e72d4d728f1393c262e
Gregory <gsh812@inbox.ru>
2023-03-06 11:02:38 +0300

    added: static data, refactored README.md

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { config } from 'dotenv';
+import * as express from 'express';
+import { join } from 'path';
 
-config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(express.static(join(__dirname, '..', 'public')));
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Server started on port ${port}`);
